393e0d16e798a9053dca4f3721f4fd649271913d
Sappakit <pipitwattana.s@gmail.com>
2024-10-28 20:48:01 +0700

    feat: complete all exercises

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 function HomePage() {
   const [products, setProducts] = useState([]);
   const [isError, setIsError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
+
+  const navigate = useNavigate();
+
+  const deleteProducts = async (productID) => {
+    try {
+      await axios.delete(`http://localhost:4001/products/${productID}`);
+      setProducts((prevProducts) =>
+        prevProducts.filter((products) => products.id !== productID)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const getProducts = async () => {
     try {
@@ -21,11 +35,14 @@ function HomePage() {
   useEffect(() => {
     getProducts();
   }, []);
+
   return (
     <div>
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
-        <button>Create Product</button>
+        <button onClick={() => navigate("/product/create")}>
+          Create Product
+        </button>
       </div>
       <div className="product-list">
         {products.map((product) => {
@@ -44,12 +61,27 @@ function HomePage() {
                 <h2>Product price: {product.price}</h2>
                 <p>Product description: {product.description} </p>
                 <div className="product-actions">
-                  <button className="view-button">View</button>
-                  <button className="edit-button">Edit</button>
+                  <button
+                    className="view-button"
+                    onClick={() => navigate(`/product/view/${product.id}`)}
+                  >
+                    View
+                  </button>
+                  <button
+                    className="edit-button"
+                    onClick={() => navigate(`/product/edit/${product.id}`)}
+                  >
+                    Edit
+                  </button>
                 </div>
               </div>
 
-              <button className="delete-button">x</button>
+              <button
+                className="delete-button"
+                onClick={() => deleteProducts(product.id)}
+              >
+                x
+              </button>
             </div>
           );
         })}
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+
 function ViewProductPage() {
+  const { productId } = useParams();
+  const [product, setProduct] = useState({});
+  const navigate = useNavigate();
+
+  async function getProduct() {
+    try {
+      const productData = await axios.get(
+        `http://localhost:4001/products/${productId}`
+      );
+      setProduct({ ...productData.data.data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, [productId]);
+
   return (
     <div>
       <h1>View Product Page</h1>
       <div className="view-product-container">
-        <h2>Product Title</h2>
-        <p>Content</p>
+        <h2>{`Name: ${product.name}`}</h2>
+        <p>{`${product.price} THB`}</p>
+        <p>{product.description}</p>
       </div>
-      <button>Back to Home</button>
+      <button onClick={() => navigate("/")}>Back to Home</button>
     </div>
   );
 }
